0ef6245e2b683f280631ce195d6bcd243542ea40
Vishanth Hari Raj <38405533+vishanth10@users.noreply.github.com>
2024-02-29 19:19:36 -0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,10 +3,10 @@ from setuptools import setup, find_packages
 
 setup(
     name='statistics_library',
-    version='1.5.3',
+    version='1.5.4',
     author='Vishanth Hari Raj Balasubramanian',
     author_email='rbvish1007@gmail.com',
-    description='A custom statistics library/ reshuffing technqiues for chi-abs, boostrapting analysis and other statistical functions.',
+    description='A custom statistics library/ resampling technqiues for chi-abs, boostrapting analysis and other statistical functions.',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
     url='https://github.com/vishanth10/statistics_library.git',
