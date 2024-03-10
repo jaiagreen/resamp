17309c7802caa0ccd4a230aa1aae18586488d1a6
Vishanth Hari Raj <38405533+vishanth10@users.noreply.github.com>
2024-03-09 16:48:22 -0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='statistics_library',
+    name='resamp',
     version='1.5.5',
     author='Vishanth Hari Raj Balasubramanian',
     author_email='rbvish1007@gmail.com',
