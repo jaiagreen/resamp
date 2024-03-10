1d9b66f08ced158a6e972e137d650e1d42904fcf
Vishanth Hari Raj <38405533+vishanth10@users.noreply.github.com>
2024-03-09 20:00:25 -0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='resamp',
-    version='1.6.1',
+    version='1.6.2',
     author='Vishanth Hari Raj Balasubramanian',
     author_email='rbvish1007@gmail.com',
     description='A custom statistics library/ resampling technqiues for chi-abs, boostrapting analysis and other statistical functions.',
